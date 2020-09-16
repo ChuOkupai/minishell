af1c7caf82ce21b8c85806bfd5e5ccd4fa64a952
Adrien SOURSOU <asoursou@student.42.fr>
2020-09-16 20:24:27 +0200

    init fix

--- src/environment/msh_env_new.c
+++ src/environment/msh_env_new.c
@@ -6,7 +6,7 @@
 /*   By: asoursou <asoursou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/31 18:21:32 by asoursou          #+#    #+#             */
-/*   Updated: 2020/09/16 20:12:50 by asoursou         ###   ########.fr       */
+/*   Updated: 2020/09/16 20:18:45 by asoursou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ t_list	*msh_env_new(char **env)
 	l = NULL;
 	while (*env)
 		if (!(s = ft_strdup(*env++)) || !ft_list_push(&l, ft_list_new(s)))
-			break ;
+			msh_abort("init");
 	msh_env_set(&l, "SHELL", MSH);
 	return (ft_list_rev(l));
 }
